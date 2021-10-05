99de05072b4dff7123ba4d54235a2cfc290493e6
James <bannis67@hotmail.com>
2021-10-06 07:55:36 +1000

    Upgrade to model factory classes

--- database/factories/SocialTokenFactory.php
+++ database/factories/SocialTokenFactory.php
@@ -1,17 +1,33 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+namespace Database\Factories;
 
 use App\SocialToken;
 use App\User;
-use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
-$factory->define(SocialToken::class, function (Faker $faker) {
-    return array(
-        'user_id' => factory(User::class),
-        'token' => Str::random(32),
-        'refresh_token' => Str::random(32),
-        'expires_at' => now()->addSeconds(60),
-    );
-});
+class SocialTokenFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = SocialToken::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'token' => Str::random(32),
+            'refresh_token' => Str::random(32),
+            'expires_at' => now()->addSeconds(60),
+        ];
+    }
+}
